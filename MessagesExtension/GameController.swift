b9e85b4e77763a47c0b8737771720e9df94eccd8
jim <whaleking168@gmail.com>
2016-09-21 16:42:32 -0700

    tiles snap to grid

--- MessagesExtension/GameController.swift
+++ MessagesExtension/GameController.swift
@@ -10,14 +10,15 @@ import Foundation
 import Firebase
 import FirebaseStorage
 import UIKit
-
+private var tiles = [TileView]()
+private var targets = [TargetView]()
 class GameController {
     var ScreenWidth = UIScreen.main.bounds.size.width
     var ScreenHeight = UIScreen.main.bounds.size.height
     var gameView: UIView!
     var level: Level!
-    private var tiles = [TileView]()
-    private var targets = [TargetView]()
+    
+    
     var photoUrl: String!
     var wordLength: Int!
     var answer: String!
@@ -39,7 +40,7 @@ class GameController {
         targets = []
         
         //create targets
-        for (index, letter) in Array(self.answer.characters).enumerated() {
+        for (index, letter) in Array(self.answer.uppercased().characters).enumerated() {
             
                 let target = TargetView(letter: letter, sideLength: tileSide)
                 target.center = CGPoint(x: xOffset + CGFloat(index)*(tileSide + 20),y: ScreenHeight/4*3-tileSide-50)
@@ -59,6 +60,7 @@ class GameController {
             } else {
                 tile.center = CGPoint(x: xOffset + CGFloat(index - 5)*(tileSide + 20),y: ScreenHeight/4*3 + tileSide + 20)
             }
+                tile.dragDelegate = self
             
             
                 //4
@@ -80,4 +82,66 @@ class GameController {
 
     
 }
-
+extension GameController:TileDragDelegateProtocol {
+    //a tile was dragged, check if matches a target
+    func tileView(tileView: TileView, didDragToPoint point: CGPoint) {
+        var targetView: TargetView?
+        for tv in targets {
+            if tv.frame.contains(point) && !tv.isMatched {
+                targetView = tv
+                break
+            }
+            
+        }
+        if let targetView = targetView {
+            
+            //2 check if letter matches
+            if String(targetView.letter) == tileView.letter {
+            
+                self.placeTile(tileView: tileView, targetView: targetView)
+                //more stuff to do on success here
+                
+                self.checkForSuccess()
+            } else {
+                
+                //4
+                self.placeTile(tileView: tileView, targetView: targetView)
+                
+                //more stuff to do on failure here
+            }
+        }
+    }
+    
+    func placeTile(tileView: TileView, targetView: TargetView) {
+        //1
+        targetView.isMatched = true
+        tileView.isMatched = true
+        
+        //2
+        tileView.isUserInteractionEnabled = true
+        
+        //3
+        UIView.animate(withDuration: 0.10,
+                                   delay:0.00,
+                                   options:UIViewAnimationOptions.curveEaseOut,
+                                   //4
+            animations: {
+                tileView.center = targetView.center
+                tileView.transform = CGAffineTransform.identity
+            },
+            //5
+            completion: {
+                (value:Bool) in
+                targetView.isHidden = true
+        })
+    }
+    func checkForSuccess() {
+        for targetView in targets {
+            //no success, bail out
+            if !targetView.isMatched {
+                return
+            }
+        }
+        NSLog("Game Over!")
+    }
+}
--- MessagesExtension/TileView.swift
+++ MessagesExtension/TileView.swift
@@ -8,7 +8,10 @@
 
 import Foundation
 import UIKit
-
+protocol TileDragDelegateProtocol {
+    func tileView(tileView: TileView, didDragToPoint: CGPoint)
+}
+private var tempTransform: CGAffineTransform = CGAffineTransform.identity
 //1
 class TileView:UIImageView {
     //2
@@ -17,7 +20,7 @@ class TileView:UIImageView {
     private var yOffset: CGFloat = 0.0
     //3
     var isMatched: Bool = false
-    
+    var dragDelegate: TileDragDelegateProtocol?
     //4 this should never be called
     required init(coder aDecoder:NSCoder) {
         fatalError("use init(letter:, sideLength:")
@@ -25,7 +28,7 @@ class TileView:UIImageView {
     
     //5 create a new tile for a given letter
     init(letter:String, sideLength:CGFloat) {
-        self.letter = letter
+                self.letter = letter
         
         //the tile background
         let image = UIImage(named: "tiles/\(self.letter).png")!
@@ -39,13 +42,27 @@ class TileView:UIImageView {
         self.frame = CGRect(x: 0, y: 0, width: image.size.width*scale, height: image.size.height*scale)
         self.isUserInteractionEnabled = true
         //more initialization here
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 0
+        self.layer.shadowOffset = CGSize(width: 10.0, height: 10.0)
+        self.layer.shadowRadius = 15.0
+        self.layer.masksToBounds = false
+        
+        let path = UIBezierPath(rect: self.bounds)
+        self.layer.shadowPath = path.cgPath
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first{
+            self.layer.shadowOpacity = 0.8
             let point = touch.location(in: self.superview)
             xOffset = point.x - self.center.x
             yOffset = point.y - self.center.y
+            tempTransform = self.transform
+            //enlarge the tile
+            self.transform = self.transform.scaledBy(x: 1.2, y: 1.2)
+
 
         }
     }
@@ -59,8 +76,15 @@ class TileView:UIImageView {
     }
     //2
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.layer.shadowOpacity = 0.0
         self.touchesMoved(touches, with: event)
+        dragDelegate?.tileView(tileView: self, didDragToPoint: self.center)
+        self.transform = tempTransform
     }
     //3
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.transform = tempTransform
+        self.layer.shadowOpacity = 0.0
+    }
     
 }
